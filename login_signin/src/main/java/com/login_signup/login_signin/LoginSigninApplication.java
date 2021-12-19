03dd14581595fb6665018da25291784ada01c2a5
Deeksha Jindal <deekshajindal058@gmail.com>
2021-12-19 10:42:58 +0530

    first commit

---
+++ login_signin/src/main/java/com/login_signup/login_signin/LoginSigninApplication.java
@@ -0,0 +1,13 @@
+package com.login_signup.login_signin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSigninApplication.class, args);
+	}
+
+}
---
+++ login_signin/src/test/java/com/login_signup/login_signin/LoginSigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login_signup.login_signin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
